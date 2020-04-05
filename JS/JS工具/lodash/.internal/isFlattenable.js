388e2d9ae1e2372b7a0d868e41e25f4e07471809
huyaocode <2385249676@qq.com>
2020-04-05 10:48:35 +0800

    lodash: 抄写lodash

---
+++ JS/JS工具/lodash/.internal/isFlattenable.js
@@ -0,0 +1,18 @@
+import isArguments from '../lang/isArguments.js'
+
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Symbol/isConcatSpreadable
+const spreadableSymbol = Symbol.isConcatSpreadable
+
+/**
+ * Checks if `value` is a flattenable `arguments` object or array.
+ *
+ * @private
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is flattenable, else `false`.
+ */
+function isFlattenable(value) {
+  return Array.isArray(value) || isArguments(value) ||
+    !!(value && value[spreadableSymbol])
+}
+
+export default isFlattenable
